e838c71bb581efe64e6b22dbe1dedadd659c0115
Ashish Jogi <anj180190@gmail.com>
2025-05-19 18:12:45 +0530

    refactor: handle multiple numbers using split and reduce

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,16 +3,11 @@ function add(numbers){
     //if number is empty 
     if(numbers==="") return 0;
     
-    //split the string into array
-    const parts = numbers.split(",");
-   
-    //Convert the array of string to  array of numbers
-    const nums = parts.map((n)=>parseInt(n));
+    //split numbers + convert the array of string to  array of numbers
+    const nums = numbers.split(",").map((n)=>parseInt(n,10));
 
     //Get the total
-    const result = nums.reduce((sum,current)=>sum+current,0);
-
-    return result;
+    return  nums.reduce((sum,current)=>sum+current,0);
 }
 
 module.exports = add;
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -13,3 +13,7 @@ test("return number for a single input",()=>{
 test("return sum for two numbers",()=>{
     expect(add("10,30")).toBe(40);
 });
+
+test("return sum of muliple comma-sparated numbers",()=>{
+    expect(add("10,20,040")).toBe(70);
+})
