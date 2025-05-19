21ae60734cf927932c848a1ea9517b5e5ba247a8
Ashish Jogi <anj180190@gmail.com>
2025-05-19 17:40:50 +0530

    feat: return number for a single input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,8 +2,7 @@
 function add(numbers){
     //if number is empty 
     if(numbers==="") return 0;
-
-
+    return parseInt(numbers);
 }
 
 module.exports = add;
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,3 +4,7 @@ const add = require("../src/stringCalculator.js");
 test("return 0 given an empty string",()=>{
     expect(add("")).toBe(0);
 });
+
+test("return number for a single input",()=>{
+    expect(add("5")).toBe(5);
+});
