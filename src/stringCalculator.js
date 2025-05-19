3e87caec4d531ec3a4cd0e7ecdb30eb0d7895ac4
Ashish Jogi <anj180190@gmail.com>
2025-05-19 18:22:30 +0530

    refactor: support both commas and newlines as delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,9 +2,12 @@
 function add(numbers){
     //if number is empty 
     if(numbers==="") return 0;
+
+    //Replace the newline with the comma to unify delimiters
+    const unified = numbers.replace(/\n/g,",");
     
     //split numbers + convert the array of string to  array of numbers
-    const nums = numbers.split(",").map((n)=>parseInt(n,10));
+    const nums = unified.split(",").map((n)=>parseInt(n,10));
 
     //Get the total
     return  nums.reduce((sum,current)=>sum+current,0);
